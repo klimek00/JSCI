d56d30ec58931195fdc49515ea91eb1796a10a1d
torxnado <36035343+torxnado@users.noreply.github.com>
2019-01-04 10:52:59 +0100

    Create tips.php

---
+++ pai-projekt/tips.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+        include 'head_HTML.html';
+    ?>
+</head>
+<body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Wskazówki do życia</h5>   
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Oblicz BMI</a>
+            <a class="p-2 text-dark" href="index.php">Strona główna</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Zaloguj się!</a>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h1 class="display-4">Zycie i smierc</h1>
+            <p class="text-dark">Ananas leczy nadwage!!!</p>
+        </div>
+        <div class="col-md-4 col-md-offset-5">
+            <h1 class="display-4">Naukowcy zawstydzeni</h1>
+            <p class="text-dark">Odkryl jeden sposob na...<a href="#">ZOBACZ ZDJĘCIA</a> </p>
+        </div>
+        <div class="col-md-4">
+            <h1 class="display-4">Towarzysze wspomagają</h1>
+            <p class="text-dark">Zainwestuj w psa i zdobądź tabletki odchudzjące!</p>
+        </div>
+    </div>
+    
+    
+</div>
+</body>
+</html>

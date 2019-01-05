4151e59b5094024f50911da03f4dc2a057cc14d0
jkrzywniak <46369217+jkrzywniak@users.noreply.github.com>
2019-01-06 00:19:03 +0100

    1

--- pai-projekt/header.php
+++ pai-projekt/header.php
@@ -3,11 +3,13 @@
           <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-3 text-dark" href="index.php">Kalkulator BMI Online</a></h5>
     <nav class="my-2 my-md-0 mr-md-3 ">
       <?php
-        $link = $_SERVER['REQUEST_URI'];
-        echo "<a class='p-3 text-dark' href='$link?page=calc'>Oblicz BMI</a>";
+        //$link = $_SERVER['REQUEST_URI'];
+        
+        //wyrzcam 'link' bo powoduje zapetlenie się adresu po kilkukrotnym kliknięciu w link; w sensie robi pai-projekt/?page=calc?page=calc?page=calc#
+        echo "<a class='p-3 text-dark' href='index.php?page=calc'>Oblicz BMI</a>";
       ?>
-      <a class="p-3 text-dark" href="#">Pokaż wskazówki</a>
+      <a class="p-3 text-dark" href="index.php">Pokaż wskazówki</a>
     </nav>
-    <a class="btn btn-outline-primary" href="#">Zaloguj się!</a>
+    <a class="btn btn-outline-primary" href="login.php">Zaloguj się!</a>
   </div>
 </header>
---
+++ pai-projekt/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+     include 'head_HTML.html';
+    ?>
+  </head>
+  <body>
+    <?php
+      include 'header.php';
+      ?>
+      <div class="container">
+      <h1 class="display-4"> Zaloguj się aby w pełni korzystać z mozliwości kalkulatora!</h1>
+      <form>
+          <div class="form-group">
+          
+            <label for="login">Wprowadź login poniżej:</label>
+              <input type="text" class="form-control" id="login">
+          </div>
+          <div class="form-group">
+               <label for="password">Wprowadź hasło poniżej:</label>
+              <input type="password" class="form-control" id="password">
+          </div>
+          <div>
+              <button type="submit" class="btn btn-primary">Zaloguj!</button> 
+          </div>
+        </form>
+          <div>
+          <h3> A jeżeli jeszcze nie masz konta to możesz je szybko założyć klikając w prycisk poniżej:</h3>
+          <a href="register.php" class="btn btn-secondary">Zarejestruj się!</a>
+      </div>
+      </div>
+      <?php
+       include 'footer.html';
+      ?>
+    </body>
+</html>

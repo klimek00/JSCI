894b9a1bb9ab063d2379e6ab1d2aad0dfc9de78b
jkrzywniak <46369217+jkrzywniak@users.noreply.github.com>
2019-01-09 17:17:35 +0100

    w

--- pai-projekt/calculator.php
+++ pai-projekt/calculator.php
@@ -1,3 +1,19 @@
 <div class="container">
-  Test
+      <h1 class="display-4 text-center"> Oblicz swoje BMI!</h1>
+      <form>
+          
+          <div class="form-group w-25 mx-auto text-center">
+            <label for="waga" class="mr-sm-2">Twoja waga: </label>
+              <input type="numeric" class="form-control mb-2 mr-sm-2" id="waga">
+          </div>
+          
+          <div class="form-group w-25 mx-auto text-center">
+               <label for="wzrost" class="mr-sm-2">Twój wzrost: </label>
+              <input type="numeric" class="form-control mb-2 mr-sm-2" id="wzrost">
+          </div>
+          
+          <div class="text-center">
+              <button class="btn btn-primary" onClick="liczBMI()">Oblicz!</button> 
+          </div>
+        </form>
 </div>

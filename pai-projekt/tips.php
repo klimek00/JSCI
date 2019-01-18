697f80d4a9e4fcc89d5d6f33fa891393387fd9a8
torxnado <36035343+torxnado@users.noreply.github.com>
2019-01-18 08:48:00 +0100

    chuj

--- pai-projekt/tips.php
+++ pai-projekt/tips.php
@@ -9,6 +9,7 @@
    <?php
       include 'header.php';
       ?>
+    <div id="demo" >
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -17,13 +18,13 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="/img/1.jpg" alt="First slide">
+            <img class="d-block" src="./img/1.jpg" alt="First slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="/img/2.jpg" alt="Second slide">
+            <img class="d-block" src="./img/2.jpg" alt="Second slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="/img/3.jpg" alt="Third slide">
+            <img class="d-block" src="./img/3.jpg" alt="Third slide">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -36,37 +37,6 @@
     </a>
 </div>
     
-    <div id="demo" class="carousel slide" data-ride="carousel">
-
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-
-  <!-- The slideshow -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/img/1.jpg" alt="Los Angeles">
-    </div>
-    <div class="carousel-item">
-      <img src="/img/2.jpg" alt="Chicago">
-    </div>
-    <div class="carousel-item">
-      <img src="/img/3.jpg" alt="New York">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-
-</div>
 
     
 </body>

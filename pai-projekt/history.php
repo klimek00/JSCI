8833fdbcd4a37ce0350afa9f27f92065028714a8
Michal582 <46369132+Michal582@users.noreply.github.com>
2019-05-15 18:50:11 +0200

    Adding the second version of the chart with the history of bmi

--- pai-projekt/history.php
+++ pai-projekt/history.php
@@ -1,23 +1,31 @@
+<?php
+session_start();
+if (isset($_SESSION["id"])) {
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
     <?php
      include 'head_HTML.html';
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0" charset="utf-8"></script>
   </head>
-  <body>
+  <body onload="draw()">
     <?php
       include 'header.php';
     ?>
-    <div class="container border" style="margin-top: 100px;">
-
+    <div class="container border" style="margin-top: 100px">
     <?php
-      include 'wykres.php'; 
+      // include 'wykres.php';
+      include 'chartjsHistory.php';
     ?>
-        
+
     </div>
     <?php
       include 'footer.html';
     ?>
   </body>
 </html>
+<?php } else {
+  header("Location: index.php");
+} ?>

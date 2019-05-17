e7a909520e40b626e9020512cc61a10233c2b872
jkrzywniak <46369217+jkrzywniak@users.noreply.github.com>
2019-05-17 16:18:05 +0200

    Nowości na stronie głównej

--- pai-projekt/advertisement.php
+++ pai-projekt/advertisement.php
@@ -4,6 +4,27 @@
     height: 100%;
   }
 </style>
+
+
+<div class="container-fluid text-center text-primary jumbotron jumbotron-fluid bg-white" style="margin-top:70px;">
+    <h1><small>Twoje zdrowie jest dla Nas bardzo ważne!</small></h1> <br>
+    <h2 class="mt-4 mb-4"><small>Dlatego pragniemy Ci przedstawić aplikację do kontroli swojego ciała</small></h2><br>
+    <span class="display-4 mt-4">To jeszcze nigdy nie było takie łatwe!</span>
+    
+</div>
+<div class="bg-primary container-fluid text-center text-primary jumbotron jumbotron-fluid lead">
+    <span class="text-white lead" style="font-size: 25px;">Część funkcji naszej aplikacji możesz przetesować pozostająć Gościem!</span><br>
+    <a class="m-4 btn btn-outline-light w-25" href="calculator.php" data-toggle="modal" data-target="#theModal">Oblicz BMI</a>
+    <a class="m-4 btn btn-outline-light w-25" href="calcKCAL.php">Oblicz zapotrzebowanie kaloryczne</a><br>
+    <span class="text-white lead m-4" style="font-size: 25px;">Jednak część funkcjonalności wymaga zalogowania się!</span><br>
+    <a href="#zalety" class="text-white m-4">Zobacz co możesz zyskać rejestrując się!</a><br>
+    <a class="m-4 btn btn-outline-light w-25" href="login.php">Jeżeli masz już konto - Zaloguj się</a>
+    <a class="m-4 btn btn-outline-light w-25" href="register.php">Jeżeli jeszcze go nie masz - Załóż je</a><br>
+</div>
+<div class="bg-white container-fluid text-center text-primary jumbotron jumbotron-fluid lead" id="zalety">
+    sfdsd
+</div>
+<!--
 <div class="container-fluid" style="margin-top: 80px;">
   <div id="demo" class="carousel slide" data-ride="carousel">
     <ul class="carousel-indicators">
@@ -24,7 +45,7 @@
       </div>
     </div>
 
-    <!-- Left and right controls -->
+     Left and right controls 
     <a class="carousel-control-prev" href="#demo" data-slide="prev">
       <span class="carousel-control-prev-icon"></span>
     </a>
@@ -33,3 +54,4 @@
     </a>
   </div>
 </div>
+-->

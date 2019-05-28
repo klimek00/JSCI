34676a6a09040e44cc1d5c1e3f0a7b1c48e24c6d
Michal582 <46369132+Michal582@users.noreply.github.com>
2019-05-28 18:14:29 +0200

    Update tips

--- pai-projekt/tips/prawidlowaTips.php
+++ pai-projekt/tips/prawidlowaTips.php
@@ -1,5 +1,37 @@
-<div class="container-fluid text-center text-primary jumbotron jumbotron-fluid bg-white" style="margin-top:70px;">
-    <h1><small>Twoja waga jest odpowiednia</small></h1> <br>
-    <h2 class="mt-4 mb-4"><small>Tu w przyszłości będą wskazówki dla Ciebie</small></h2><br>
-    
+<div class="container-fluid text-center" style="margin-top:70px;">
+  <h1 class="text-primary mb-5">Według ostatniego pomiaru, posiadasz prawidłową wagę</h1>
+  <h5 class="lead">Jeżeli chcesz utrzymać prawidłową wagę, konieczne jest prowadzenie zdrowego trybu życia. Przede wszystkim powinieneś zrezygnować ze starych nawyków żywieniowych. <br /> Poniżej prezentujemy porady na utrzymanie prawidłowej masy ciała</h5>
+  <div class="container mt-5">
+  <div class="card-columns d-flex">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+      </div>
+    </div>
+  </div>
+  <div class="card-columns d-flex">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+      </div>
+    </div>
+  </div>
+  <div class="card-columns center">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+      </div>
+    </div>
+  </div>
+</div>
 </div>

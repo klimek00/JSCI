e64b0f759337bb994e10e5767b9187cd7c03007f
torxnado <36035343+torxnado@users.noreply.github.com>
2019-05-28 23:02:48 +0200

    adminPanel-ready to fly
    jacek, gdzie tłumaczenie?

--- pai-projekt/delUser.php
+++ pai-projekt/delUser.php
@@ -1,18 +1,84 @@
+<?php session_start();
+if(isset($_GET['id']) && isset($_SESSION['id'])) {
+    $idDel = $_GET['id'];
+    $id = $_SESSION['id'];
+?>
 <div class="modal-header">
-  <h3 class="modal-title"> Are you sure that you want to delete user $id?</h3>
+  <h3 class="modal-title"> Are you sure that you want to delete user ID: <?php echo $idDel; ?> ?</h3>
   <button type="button" class="close" data-dismiss="modal">X</button>
 </div>
 <div class="modal-body">
   <div class="form-group mx-auto text-center">
-      <label for="sex" class="mr-sm-2">Twoja płeć:</label>
-      <div class="radio">
-        <label><input type="radio" name="sex" id="m"> Mężczyzna</label>
-      </div>
-      <div class="radio">
-        <label><input type="radio" name="sex" id="w"> Kobieta</label>
-      </div>
+      <?php echo "<form action='delUser.php?id=$idDel' method='post'>"; ?>
+          <input type="submit" name='btn' value="Yess" class='btn btn-outline-dark'>
+          <input type="submit" name='btn' value="HeckNo" class='btn btn-outline-dark'>
+      </form>
   </div>
 </div>
 <div class="modal-footer">
   <button type="button" class="btn btn-danger" data-dismiss="modal">Zamknij</button>
 </div>
+<?php       
+}
+else {
+    ?>
+<div class="modal-header">
+        <h1 class="modal-title">WARNING!</h1>
+</div>
+<div class="modal-body">
+  <div class="form-group mx-auto text-center">
+      <h1><small>YOUR ACTIVITY HAS BEEN SAVED TO DATABASE AND WILL BE SENT TO GOVERMENT OF YOUR COUNTRY. IF YOU HAVE DONE THAT BY A MISTAKE, PLEASE CLOSE THIS WINDOW IMMEDIATELY. ( ͡° ͜ʖ ͡°)</small></h1>
+  </div>
+</div>
+<div class="modal-footer"></div>
+<?php
+}
+if(isset($_POST['btn']) && $_POST['btn'] == 'Yess') {
+    try {
+      $db = new PDO("sqlite:database.sqlite");
+    } catch (PDOException $e) {
+      echo "Error!".$e->getMessage();
+    } 
+    $query = "SELECT * FROM users WHERE ID = $id AND perm = 'admin'";
+    $result = $db->query($query);
+      if (!$result) {
+          header("Location: index.php");
+      } else {
+          $rows = $result->fetch();
+          if ($rows) {
+           try {
+               $sql = "DELETE FROM users WHERE ID = $idDel";
+               $result = $db->query($sql);
+               if (!$result) {
+                   echo "<SCRIPT type='text/javascript'>
+                         alert('Error!!!');
+                         window.location.replace(\"adminPanel.php\");
+                         </SCRIPT>";
+               } else {
+                   $sql2 = "DELETE FROM chart WHERE IDUser = $idDel";
+                   $result = $db->query($sql2);
+                   if (!$result) {
+                       echo "<SCRIPT type='text/javascript'>
+                             alert('Error!!!');
+                             window.location.replace(\"adminPanel.php\");
+                             </SCRIPT>";
+                   } else {
+                       header("location: adminPanel.php?action=success"); 
+                   }
+               }
+           } catch (PDOException $e) {
+               echo "Error!".$e->getMessage();
+           }  
+            
+          }
+      }
+    $db = NULL;
+} else if (isset($_POST['btn']) && $_POST['btn'] == 'HeckNo') {
+    header("location: adminPanel.php");   
+}
+?>
+<script type="text/javascript">
+    $('#deleteModal').on('hidden.bs.modal', function() {
+    $(this).removeData('bs.modal');
+});
+</script>
--- pai-projekt/editUser.php
+++ pai-projekt/editUser.php
@@ -1,62 +1,103 @@
- <?php
-  if (isset($_SESSION["id"])) {
-    $id = $_SESSION["id"];
+<?php session_start();
+if(isset($_GET['id']) && isset($_SESSION['id'])) {
+    $idEdit = $_GET['id'];
+    $id = $_SESSION['id'];
+    
     try {
       $db = new PDO("sqlite:database.sqlite");
     } catch (PDOException $e) {
       echo "Error!".$e->getMessage();
-    }
-      $query = "SELECT * FROM users WHERE ID = $id AND perm = 'admin'";
-      $result = $db->query($query);
-      if ($result === false) {
-        header("Location: index.php");
+    } 
+    $q = "SELECT * FROM users WHERE ID = $idEdit";
+    $result = $db->query($q);
+?>
+<div class="modal-header">
+  <h3 class="modal-title"> Change details of user ID: <?php echo $idEdit; ?> ?</h3>
+  <button type="button" class="close" data-dismiss="modal">X</button>
+</div>
+<div class="modal-body">
+  <div class="form-group mx-auto text-center">
+      <?php 
+        foreach($result as $item) {
+            echo "
+            <form action='editUser.php?id=$idEdit' method='post'>
+              <label for='login'>Login:</label>
+              <input type='text' class='form-control' name='login' value='".$item['login']."'>
+              <label for='email'>e-mail:</label>
+              <input type='email' class='form-control' name='email' value='".$item['email']."'>
+              <label for='dateOfBirth'>Date of birth:</label>
+              <input type='date' class='form-control' name='dateOfBirth' value='".$item['dateOfBirth']."'>
+              <label for='gender'>Gender:</label>
+              <input type='text' class='form-control' name='gender' value='".$item['gender']."'>
+              <label for='permission'>Permission:</label>
+              <input type='text' class='form-control' name='permission' value='".$item['perm']."'><br>
+              <input type='submit' name='submit' value='Update' class='btn btn-outline-dark'>
+          </form>
+            ";
+        }
+    ?>
+  </div>
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-danger" data-dismiss="modal">Zamknij</button>
+</div>
+<?php       
+}
+else {
+    ?>
+<div class="modal-header">
+        <h1 class="modal-title">WARNING!</h1>
+</div>
+<div class="modal-body">
+  <div class="form-group mx-auto text-center">
+      <h1><small>YOUR ACTIVITY HAS BEEN SAVED TO DATABASE AND WILL BE SENT TO GOVERMENT OF YOUR COUNTRY. IF YOU HAVE DONE THAT BY A MISTAKE, PLEASE CLOSE THIS WINDOW IMMEDIATELY. ( ͡° ͜ʖ ͡°)</small></h1>
+  </div>
+</div>
+<div class="modal-footer"></div>
+<?php
+}
+if(isset($_POST['submit'])) {
+    if($_POST['login'] != "")
+        $newLogin = $_POST['login'];
+    if($_POST['email'] != "")    
+        $newEmail = $_POST['email'];
+    $newDateOfBirth = $_POST['dateOfBirth'];
+    $newGender = $_POST['gender'];
+    $newPermission = $_POST['permission'];
+    
+    try {
+      $db = new PDO("sqlite:database.sqlite");
+    } catch (PDOException $e) {
+      echo "Error!".$e->getMessage();
+    } 
+    $query = "SELECT * FROM users WHERE ID = $id AND perm = 'admin'";
+    $result = $db->query($query);
+      if (!$result) {
+          header("Location: index.php");
       } else {
-        $rows = $result->fetch();
-        if ($rows != false) {
+          $rows = $result->fetch();
+          if ($rows) {
+               try {
+                   $sql = "UPDATE `users` SET `login`='$newLogin',`email`='$newEmail',`dateOfBirth`='$newDateOfBirth',`gender`='newGender',`perm`='$newPermission' WHERE id=$idEdit";
+                   $result = $db->query($sql);
+                   if (!$result) {
+                       echo "<SCRIPT type='text/javascript'>
+                             alert('Error!!!');
+                             window.location.replace(\"adminPanel.php\");
+                             </SCRIPT>";
+                   } else {
+                        header("location: adminPanel.php?action=success"); 
+                   }
+               } catch (PDOException $e) {
+                   echo "Error!".$e->getMessage();
+               }  
+          }
+      }
+    $db = NULL;
+}
 ?>
 <script type="text/javascript">
-  if (!confirm('Are you sure?')){
-    window.location.replace("adminPanel.php");
-  }
+    $('#editModal').on('hidden.bs.modal', function() {
+    $(this).removeData('bs.modal');
+});
 </script>
-<?php
-  try {
-    $ident = $_GET["id"];
-    $db = new PDO ("sqlite:database.sqlite");
-    $sql = "DELETE FROM users WHERE ID = $ident";
-    $result = $db->query($sql);
-    if ($result === false) {
-      echo "<SCRIPT type='text/javascript'>
-      alert('Error!!!');
-        window.location.replace(\"adminPanel.php\");
-      </SCRIPT>";
-    } else {
-      $sql2 = "DELETE FROM chart WHERE IDUser = $ident";
-      $result = $db->query($sql2);
-      if ($result === false) {
-        echo "<SCRIPT type='text/javascript'>
-        alert('Error!!!');
-          window.location.replace(\"adminPanel.php\");
-        </SCRIPT>";
-      } else {
-        echo "<SCRIPT type='text/javascript'>
-        alert('Delete success!');
-          window.location.replace(\"adminPanel.php?action=in\");
-        </SCRIPT>";
-      }
-    }
-  } catch (PDOException $e) {
-      echo "Error!".$e->getMessage();
-  }
-
-?>
-<!-- <?php
-} else {
-  header("Location: index.php");
-}
-  }
-$db = null;
-} else {
-  header("Location: index.php");
-}
-?> -->

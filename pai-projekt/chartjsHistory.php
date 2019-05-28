c18bae87f820af4f80a11263fe779147d4129877
Michal582 <46369132+Michal582@users.noreply.github.com>
2019-05-28 22:23:06 +0200

    Update chartjsHistory.php

--- pai-projekt/chartjsHistory.php
+++ pai-projekt/chartjsHistory.php
@@ -1,32 +1,33 @@
 <?php
 if (isset($_SESSION["id"])) {
- ?>
+?>
 <div class="chart-container" style="position: relative; height:10px; width:10px; margin-top: 10px">
     <canvas id="historyChart"></canvas>
 </div>
 <?php
-  $userid = $_SESSION["id"];
-  try {
-    $db = new PDO("sqlite:database.sqlite");
-  } catch (PDOException $e) {
-    echo $e->getMessage();
-  }
-  $sql = "SELECT date, bmiResult FROM chart WHERE IDUser = $userid ORDER BY ID DESC LIMIT 10";
-  $result = $db->query($sql);
-  if ($result === false) {
-    echo '
+    $userid = $_SESSION["id"];
+    try {
+        $db = new PDO("sqlite:database.sqlite");
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+    $sql = "SELECT date, bmiResult FROM chart WHERE IDUser = $userid ORDER BY ID DESC LIMIT 10";
+    $result = $db->query($sql);
+    if ($result === false) {
+        echo '
     <div class="container alert alert-danger alert-dismissible fade show" style="margin-top: 20px">
       <button type="button" class="close" data-dismiss="alert">&times;</button>
       <strong>Wystąpił problem z pobraniem wyników</strong><br>Być może nie ma jeszcze żadnych danych do wyświetlenia.
     </div>
     ';
-  } else {
-    $history = array();
-      foreach ($result as $value) {
-        $history[] = array('date' => $value["date"],'bmi' => floatval($value["bmiResult"]));
-      }
-    $history_json = json_encode($history);
-  }
+    } else {
+        $history = array();
+        foreach ($result as $value) {
+            $history[] = array('date' => $value["date"], 'bmi' => floatval($value["bmiResult"]));
+        }
+        $history_json = json_encode($history);
+    }
 ?>
 <script type="text/javascript">
 function draw() {

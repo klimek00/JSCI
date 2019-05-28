47ddbc6eefa5e0410f02dd18bcedfbb696f8e3b8
torxnado <36035343+torxnado@users.noreply.github.com>
2019-05-28 17:32:02 +0200

    Update advertisement.php

---
+++ pai-projekt/advertisement.php
@@ -0,0 +1,29 @@
+<style>
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+
+<div class="container-fluid text-center text-primary jumbotron jumbotron-fluid bg-white" style="margin-top:70px;">
+    <h1><small>Twoje zdrowie jest dla nas ogromnie ważne!</small></h1> <br>
+    <h2 class="mt-4 mb-4"><small>Dlatego pragniemy Ci przedstawić aplikację do kontroli swojego ciała</small></h2><br>
+    <span class="display-4 mt-4">To jeszcze nigdy nie było takie łatwe!</span>
+    
+</div>
+<div class="bg-primary container-fluid text-center text-primary jumbotron jumbotron-fluid lead">
+    <span class="text-white lead" style="font-size: 25px;">Korzystanie z naszej aplikacji jest możliwe będąc Gościem.</span><br>
+    <a class="m-4 btn btn-outline-light w-25" href="calculator.php" data-toggle="modal" data-target="#theModal">Oblicz BMI</a>
+    <a class="m-4 btn btn-outline-light w-25" href="calcKCAL.php">Oblicz zapotrzebowanie kaloryczne</a><br>
+    <span class="text-white lead m-4" style="font-size: 25px;">Mimo to zalecamy zalogowanie się w celu uzyskania dodatkowych funkcji!</span><br>
+    <a href="#zalety" class="text-white m-4">Zobacz co możesz zyskać rejestrując się!</a><br>
+    <a class="m-4 btn btn-outline-light w-25" href="login.php">Jeżeli masz już konto - Zaloguj się</a>
+    <a class="m-4 btn btn-outline-light w-25" href="register.php">Jeżeli jeszcze go nie masz - Załóż je</a><br>
+</div>
+<div class="bg-white container-fluid text-center text-primary jumbotron jumbotron-fluid lead" id="zalety">
+    <span class="display-4 mx-4">Poznaj siebie i budowę człowieka od środka!</span> <br><br>
+    <span class="mx-4 px-4 lead" style="font-size: 25px;">Dla zarejestrowanych użytkowników udostępniamy wykresy z historią wyników BMI: </span><br>
+    <img src="img/history.PNG" class="mx-auto d-block mx-4 w-50">
+    
+</div>

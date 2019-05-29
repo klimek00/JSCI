2a7f09f32eb8154f81f31fc2a0312c0e23b6af18
Michal582 <46369132+Michal582@users.noreply.github.com>
2019-05-29 10:21:22 +0200

    Update login.php

--- pai-projekt/login.php
+++ pai-projekt/login.php
@@ -45,11 +45,7 @@ if (!isset($_SESSION["id"])){ ?>
               } catch(PDOException $e) {
                 echo "Error!" . $e->getMessage();
               }
-          // if (strpos($passwd, "' OR ") ) {
-
-          // } else {
           $query = "SELECT id, login, passwd FROM users WHERE login = '$login' AND passwd ='$passwd'";
-        // }
           $result = $db->query($query);
           if ($result === false) {
               echo '<div class="container alert alert-danger alert-dismissible fade show" style="margin-top: 20px">
